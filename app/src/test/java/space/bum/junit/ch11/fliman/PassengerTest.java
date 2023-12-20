2e2981d2df45e1047a610135709bc1864d8827b0
osparking <jbpark03@gmail.com>
2023-12-20 10:25:56 +0900

    승객 생성 시험 메소드

---
+++ app/src/test/java/space/bum/junit/ch11/fliman/PassengerTest.java
@@ -0,0 +1,17 @@
+package space.bum.junit.ch11.fliman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PassengerTest {
+
+  @Test
+  @DisplayName("항공 승객 정상 생성")
+  void testPassenter() {
+    Passenger passenger = new Passenger("010-1234-5678", "등산객");
+    String exPaStr = "Passenger(identifier=010-1234-5678, name=등산객)";
+    assertEquals(exPaStr, passenger.toString());
+  }
+}
