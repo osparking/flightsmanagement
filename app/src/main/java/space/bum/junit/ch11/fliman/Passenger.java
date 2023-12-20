e965f88b05fe466d97d7ec64cd687c34e4d3036a
osparking <jbpark03@gmail.com>
2023-12-20 17:10:32 +0900

    작동하지 않는 롬복을 제거하였음

--- app/src/main/java/space/bum/junit/ch11/fliman/Passenger.java
+++ app/src/main/java/space/bum/junit/ch11/fliman/Passenger.java
@@ -1,18 +1,29 @@
 package space.bum.junit.ch11.fliman;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
-
-@AllArgsConstructor
-@Getter
-@ToString
 public class Passenger {
   private String identifier;
   private String name;
 
   public static void main(String args[]) {
-    Passenger passenger = new Passenger("123-456-789", "정 상표");
+    Passenger passenger = new Passenger("123-456-789", "정상표");
     System.out.println(passenger);
+  }
+
+  public String getIdentifier() {
+    return identifier;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Passenger(String identifier, String name) {
+    this.identifier = identifier;
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Passenger [identifier=" + identifier + ", name=" + name + "]";
   }
 }
